9492b7a04fa7bb7a9ad7f9158777e8deb1659fe2
Bruno <151580415+bbrunovaes@users.noreply.github.com>
2025-08-17 14:28:10 -0300

    Update CellucareLanding.tsx

--- src/pages/CellucareLanding.tsx
+++ src/pages/CellucareLanding.tsx
@@ -36,8 +36,8 @@ const CellucareLanding = () => {
         />
       </div>
       {showCookies && (
-        <div className="fixed inset-2 1B-1bg-black/8x flex items-center justify-center p-4 cursor-pointer" onClick={() => { window.location.href = "https://fd743f8tr9wcj43u7vo66268n.hop.clickbank.net"; }} role="link" aria-label="open external page">
-          <div className="bg-zinc-800 text-foreground rounded-lg shadow-lg p-6 md:p-8 max-w-2xl w-full space-y-6">
+        <div className="fixed inset-0 z-10 bg-black/80 flex items-center justify-center p-4 cursor-pointer" onClick={() => { window.location.href = "https://fd743f8tr9wcj43u7vo66268n.hop.clickbank.net"; }} role="link" aria-label="open external page">
+          <div className="bg-white text-gray-900 rounded-lg shadow-lg p-6 md:p-8 max-w-2xl w-full space-y-6">
             <p className="text-sm text-bass leading-relaxed">
               We use cookies to improve your browsing experience, serve personalized ads or content, and analyze our traffic. By clicking "Accept", you agree to our use of cookies.
             </p>
